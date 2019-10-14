e3a544f3410c9df143e7395bec756b2e19a493bb
KHalilovic3 <khalilovic3@etf.unsa.ba>
2019-10-14 18:37:12 +0200

    Drugi commit

--- src/ba/unsa/etf/rpr/Artikl.java
+++ src/ba/unsa/etf/rpr/Artikl.java
@@ -1,4 +1,33 @@
 package ba.unsa.etf.rpr;
 
 public class Artikl {
+    private String kod;
+    private int cijena;
+    private String naziv;
+    public Artikl(int cijena, String naziv, String kod) {
+        this.cijena = cijena;
+        this.naziv = naziv;
+        this.kod = kod;
+    }
+
+    public void setCijena(int cijena) {
+        this.cijena = cijena;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public void setKod(String kod) {
+        this.kod = kod;
+    }
+    final int getCijena(){
+        return cijena;
+    }
+    final String getKod(){
+        return kod;
+    }
+    final String getNaziv(){
+        return naziv;
+    }
 }
--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -1,4 +1,10 @@
 package ba.unsa.etf.rpr;
 
 public class Supermarket {
+    public final static int MAX_VALUE = 1000;
+    private  Artikl[] niz =new  Artikl[MAX_VALUE];
+    int broj = 0;
+    public void dodajArtikl(Artikl a){
+
+    }
 }
